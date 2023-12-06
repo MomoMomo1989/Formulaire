3b887a41d650d59a81404343bbe8b3d1c0719c8f
Momo <mohandguellal.dev@gmail.com>
2023-12-06 15:51:39 +0100

    3

--- assets/main.js
+++ assets/main.js
@@ -3,14 +3,17 @@ let btn = document.querySelector('#btn');
 let Nm = document.querySelector('#Nm');
 let lbNm = document.querySelector('#lbNm');
 let formul = document.querySelector('#formul');
+let chk = document.querySelector('#chk');
+let cke = document.querySelector('#cke');
 
-Nm.addEventListener('focus', ()=>{
+Nm.addEventListener('focus', ({target : {value} })=>{
     lbNm.style.visibility = 'visible'
     Nm.style.outline = 'none'
     Nm.style.borderWidth = '0 0 2px 0'
     Nm.style.borderColor = '#2209ff'
     lbNm.style.color = '#2209ff'
     Nm.placeholder = ""
+    
 });
 btn.addEventListener('click', ()=>{
     btn.style.backgroundColor = '#2209ff';
@@ -47,7 +50,40 @@ formul.Cnfrme.addEventListener('focus', ()=>{
     lbconfrm.style.color = 'red'
     Cnfrme.placeholder = ""
 });
+chk.addEventListener('click', ()=> {
+    cke.checked=true
+});
 
-formul.chk.addEventListener('click', ()=> {
-  alerte('cc')  
+Nm.addEventListener('input', ({target: {value } })=>{
+    
+    if (value.length < 7 ){
+        Nm.style.borderColor = '#2209ff'
+        lbNm.style.color = '#2209ff'
+    }
+    else if (value.length >= 7 ){
+        Nm.style.borderColor = '#09AB32'
+        lbNm.style.color = '#09AB32'
+    }
+});
+formul.prnm.addEventListener('input', ({target: {value } })=>{
+    
+    if (value.length < 1 ){
+        prnm.style.borderColor = '#2209ff'
+        lbprenom.style.color = '#2209ff'
+    }
+    else if (value.length >= 1 ){
+        prnm.style.borderColor = '#09AB32'
+        lbprenom.style.color = '#09AB32'
+    }
+});
+formul.mail.addEventListener('input', ({target: {value } })=>{
+    
+    if (!value.includes('@') ){
+        mail.style.borderColor = '#2209ff'
+        lbmail.style.color = '#2209ff'
+    }
+    else if (value.includes('@') ){
+        mail.style.borderColor = '#09AB32'
+        lbmail.style.color = '#09AB32'
+    }
 });
